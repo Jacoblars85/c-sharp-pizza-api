26f9cafc485848f398fadc890d7bd110def276e5
Jacob Larson <hockeybobber@comcast.net>
2024-10-09 15:52:42 -0500

    changed names of the conections

--- pizza_api/Controllers/PizzaController.cs
+++ pizza_api/Controllers/PizzaController.cs
@@ -1,8 +1,8 @@
-using ContosoPizza.Models;
-using ContosoPizza.Services;
+using Pizza_api.Models;
+using Pizza_api.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ContosoPizza.Controllers;
+namespace Pizza_api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
